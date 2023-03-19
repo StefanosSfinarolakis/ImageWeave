2215ff1921738ff439e2360e5e99f8c872fc6ab4
inf2021090 <inf2021090@ionio.gr>
2023-03-19 14:18:18 +0200

    create personal app

--- personal/apps.py
+++ personal/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProfilepageConfig(AppConfig):
+class PersonalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'profilePage'
+    name = 'personal'
