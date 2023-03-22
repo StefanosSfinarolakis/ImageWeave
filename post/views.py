68a959ddfef79d40a8c0ea4c3b827c069d75aa08
inf2021229 <inf2021229@ionio.gr>
2023-03-23 00:53:28 +0200

    Upload_Post_1.0

--- post/views.py
+++ post/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Post
 
 # Create your views here.
+# @login_required(login_url='signin')
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts':posts})
+
+# @login_required(login_url='signin')
+def upload(request):
+
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+        
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+
+    else:
+        return redirect('/')
+    
+
+    
