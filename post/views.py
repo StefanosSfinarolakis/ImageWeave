2958fc3305c7882f42f55526a32c7273d4b763cf
inf2021229 <inf2021229@ionio.gr>
2023-03-29 13:00:22 +0300

    Post Feed and Likes

--- post/views.py
+++ post/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render ,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Post
+from .models import Post, LikePost
 
 # Create your views here.
 # @login_required(login_url='signin')
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts':posts})
+
+# @login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+    
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.number_of_likes = post.number_of_likes+1
+        post.save()
+        return redirect('/home/')
+    else:
+        like_filter.delete
+        post.number_of_likes = post.number_of_likes+1
+        post.save()
+        return redirect('/home/')
 
 # @login_required(login_url='signin')
 def upload(request):
@@ -20,10 +41,10 @@ def upload(request):
         new_post = Post.objects.create(user=user, image=image, caption=caption)
         new_post.save()
 
-        return redirect('/')
+        return redirect('/home/')
 
     else:
-        return redirect('/')
+        return redirect('/home/')
     
 
     
